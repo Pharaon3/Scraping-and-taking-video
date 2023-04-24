8b95a24e6ce1e9fe7aab533572889030de7ea31d
Pharaon3 <Pharaon313@outlook.com>
2023-04-24 08:18:07 +0300

    log in

--- run.py
+++ run.py
@@ -1,3 +1,47 @@
+import sys
 from selenium import webdriver
-driver = webdriver.Chrome()
+from selenium.webdriver.common.keys import Keys
+
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+
+import time 
+from selenium.webdriver.common.action_chains import ActionChains
+
+# options = Options()
+# options.add_argument("start-maximized")
+# options.add_experimental_option('excludeSwitches', ['enable-logging'])
+# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+options = webdriver.ChromeOptions()
+# options.add_argument('headless')
+options.add_argument('window-size=1920x1080')
+options.add_argument("disable-gpu")
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), chrome_options=options)
+
+# driver.get('https://meine.postbank.de/iob5/#/login')
+
+# driver.get('https://meine.postbank.de/iob5/#/login')
+
 driver.get('https://student.emeritus.org/courses/4208/modules')
+
+mailPath = '//*[@id="student_email"]'
+mailSubmitPath = '/html/body/main/div/div/div/form/input[2]'
+passwordPath = '//*[@id="user_password"]'
+passwordSubmitPath = '//*[@id="new_user"]/input[4]'
+titlePath = '//*[@id="context_module_item_812921"]/div/div[1]/div[1]/span/a'
+
+time.sleep(1)
+driver.find_element("xpath", mailPath).send_keys('rajatnathan@icloud.com')
+driver.find_element("xpath", mailSubmitPath).click()
+time.sleep(1)
+driver.find_element("xpath", passwordPath).send_keys('deggu0-hiskaz-cebJed')
+driver.find_element("xpath", passwordSubmitPath).click()
+time.sleep(50)
+
+title = driver.find_element("xpath", titlePath).click()
+title = driver.find_element("xpath", titlePath).get_attribute('innerHTML')
+print(title)
+# driver.find_element("xpath", passwordButtonPath).send_keys(u'\ue007')
+# driver.find_element("xpath", passwordPath).send_keys(password)
